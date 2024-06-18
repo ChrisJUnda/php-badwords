0bd616f18e0c8be84d7102ee61a2a33296666bc2
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-06-18 09:53:56 +0200

    [UPD] Index.php

--- EX/index.php
+++ EX/index.php
@@ -6,6 +6,36 @@
     <title>PHP BADWORDS</title>
 </head>
 <body>
+    <form action="result.php" method="GET">
+        <div>
+            <div>
+            <label for="text">
+                Inserisci il testo
+            </label>
+
+            </div>
+            
+            
+            <textarea name="text" id="text" rows="4" cols="50">
+            </textarea>
+            <hr>
+
+            <div>
+            <label for="censure">
+                Inserisci la parola che verrà censurata:
+            </label>
+
+            </div>
+
+            
+            <input type="text" name="censure" id="censure">
+            <hr>
+            <button type="submit" value="submit">
+                Invia il modulo
+            </button>
+
+        </div>
+    </form>
     
 </body>
 </html>
